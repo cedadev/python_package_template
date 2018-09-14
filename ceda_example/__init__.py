42cc503a8bf001c299ab3bae8100f389c491478a
Philip Kershaw <philip.kershaw@stfc.ac.uk>
2018-09-14 18:04:36 +0100

    Initial version - needs tidying up

---
+++ ceda_example/__init__.py
@@ -0,0 +1,9 @@
+'''CEDA Example package
+'''
+__author__ = "Philip Kershaw"
+__date__ = "14 Sep 2018"
+__copyright__ = "(C) 2018 Science and Technology Facilities Council"
+__license__ = "${license}"
+__contact__ = "Philip.Kershaw@stfc.ac.uk"
+__all__ = []
+__version__ = "0.1.0"
---
+++ ceda_example/http_client.py
@@ -0,0 +1,56 @@
+'''ceda_example.http_client - simple example class for reading HTTP content
+'''
+__author__ = "Philip Kershaw"
+__date__ = "14 Sep 2018"
+__copyright__ = "(C) 2018 Science and Technology Facilities Council"
+__license__ = "BSD - see LICENSE file in top-level package directory"
+__contact__ = "Philip.Kershaw@stfc.ac.uk"
+__all__ = ['HttpClient']
+from http import HTTPStatus # system package imports first
+import logging
+
+import requests # then 3rd party package imports
+
+# Then imports from this package here if needed
+
+# Then code
+log = logging.getLogger(__name__)
+
+
+class HttpClientError(Exception):
+    """Base class for CEDA Example HTTP Client exceptions"""
+    
+
+class HttpClientReadFileError(HttpClientError):
+    """Error reading file from given link"""
+    
+ 
+# For Python 2, inherit from object so that you can get new-style class
+# features.  In Python 3 this is not necessary   
+class HttpClient: 
+    '''Simple HTTP client class for illustration
+    '''
+    HTTP_CHUNK_SIZE = 1024
+    
+    def __init__(self, uri):
+        '''Set the URI to operate on
+        '''
+        self.uri = uri
+        
+    def download_file(self, output_filepath):
+        """HTTP GET a file from a given URI
+        """
+        response = requests.get(self.uri)
+        
+        if response.status_code != HTTPStatus.OK:
+            raise HttpClientReadFileError("Error downloading file from {!r}. "
+                                          " status code is {!r}".format(
+                                                      self.uri, 
+                                                      response.status_code))
+        
+        with open(output_filepath, 'wb') as file_out:
+            for chunk in response.iter_content(
+                                            chunk_size=self.HTTP_CHUNK_SIZE): 
+                if chunk:
+                    file_out.write(chunk)
+        
