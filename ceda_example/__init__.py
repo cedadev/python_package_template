a1eb9eab91367b5468755100a0fc12db65dbab79
Philip Kershaw <philip.kershaw@stfc.ac.uk>
2018-09-25 15:03:17 +0200

    Removed __all__ from __init__.py - not needed.

--- ceda_example/__init__.py
+++ ceda_example/__init__.py
@@ -5,5 +5,4 @@ __date__ = "14 Sep 2018"
 __copyright__ = "(C) 2018 Science and Technology Facilities Council"
 __license__ = "${license}"
 __contact__ = "Philip.Kershaw@stfc.ac.uk"
-__all__ = []
 __version__ = "0.1.0"
