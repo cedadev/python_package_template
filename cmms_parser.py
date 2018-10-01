0d9c59e64a83f6d166a805e23906226e5bcfeb26
Graham Parton <gparton@catalogue-test.ceda.ac.uk>
2018-10-01 16:51:42 +0100

    first bit of code to test

---
+++ cmms_parser.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+"""
+cmms_parser.py
+======================
+
+A parsing script to read in YAML files from the CEDA Manual Metadata Store (CMMS).
+
+It will :
+ - check that the requested YAML file exists in the CMMS, else return empty response
+ - If it exists it will read in the contents and check for valid YAML
+ - then test each item to make sure that it conforms to the requirements for each CMMS field
+ 
+ 
+python cmms_parser.py <uuid>
+
+ 
+Written by: G A Parton
+
+Creation: 01 Oct 2018
+
+Version 1.0: initial scripting
+
+
+"""
+# first let's call in the standard libraries typically needed when interacting with MOLES stuff...
+
+import re
+import calendar
+import datetime
+import codecs
+#from ruamel import yaml
+import yaml
+import collections
+import requests
+
+
+uuid = '455f0dd48613dada7bfb0ccfcb7a7d41' #midas - CMMS entry
+uuid = '220a65615218d5c9cc9e4785a3234bd0' #midas collection - no CMMS entry
+
+
+cmms_url = 'https://raw.githubusercontent.com/cedadev/cmms/master/yaml_files/%s.yml'
+
+def read_in_cmms(uuid):
+    '''
+    function to read in yaml file
+    '''
+    import urllib
+    txt = urllib.urlopen(target_url).read()
+
+def cmms_entry_exists(uuid):
+    '''
+    check for existance of CEDA YAML file for the submitted uuid
+    '''
+    r = requests.get(cmms_url% uuid)
+    import pdb;pdb.set_trace()
+
+
+'''
+for uuid, phenoms_list in fbi_obs_to_phenoms.items():
+    with open(os.path.join(output_dir, '%s.yml'% uuid), 'w') as outfile:
+        
+        #yaml_contents = {'splice rules': {'phenomena' : 'cmms_only'}}
+        yaml_contents = {'splice rules' : {'phenomena' : 'append_new_only'}}
+        
+        #yaml_contents.update(phenoms_list)
+        
+        #for yaml_content in yaml_contents.items():
+        yaml.dump(yaml_contents, outfile, default_flow_style=False)#, Dumper=ruamel.yaml.RoundTripDumper)
+        yaml.dump(phenoms_list, outfile, default_flow_style=False)#, Dumper=ruamel.yaml.RoundTripDumper)
+
+s'''
