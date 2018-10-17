1d82b330c3d74cc76a8ac54f2007ccb85a867a0d
Graham Parton <graham.parton@stfc.ac.uk>
2018-10-17 10:59:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         ],
     },
     install_requires=['requests',
-                      'yaml',
+                      'pyYAML',
                       'yamllint'],
     
     # This qualifier can be used to selectively exclude Python versions - 
