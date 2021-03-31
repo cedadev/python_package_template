bf865393df1b34c79cae31cc02173e8814792d93
gap736uk <graham.parton@stfc.ac.uk>
2021-03-31 11:50:25 +0100

    altering key dict to list to allow appening of additional item

--- cmms/client/parser.py
+++ cmms/client/parser.py
@@ -143,8 +143,9 @@ class CMMSParser(object):
                           'replace_append',
                           'append_new_only']
         splice_error_msg = ''
+        #import pdb;pdb.set_trace()
         for splice_item_name, splice_rule in self.yaml_content['splice rules'].items():
-            if splice_item_name.lower() in self.field_mappings.keys().append('default'):
+            if splice_item_name.lower() in list(self.field_mappings.keys()) + ['default']:
                 if splice_item_name in self.yaml_content:
                     if splice_rule in splice_options:
                         if self.field_mappings['splice rules'] in self.content:
